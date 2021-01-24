6f078f4d999180a3615b5eebd1d78d46744473c0
psurya99 <suryaprakashpadala@gmail.com>
2021-01-24 15:06:51 +0530

    package.json changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-spacex'));
+app.use(express.static(__dirname + '/dist/viewUsage'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('index.html',{root: '/dist/demo-spacex/'});
+res.sendFile('index.html',{root: '/dist/viewUsage/'});
 });
 
 // Start the app by listening on the default Heroku port
